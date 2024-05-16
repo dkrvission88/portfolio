2201c2af73993a43cc5559699022e57e0beea626
DKRVISSION <dkrvission88@gmail.com>
2024-05-16 18:21:12 +0530

    jdnj

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Intouch from './Components/Intouch';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Nav/>
+    <Home/>
+    <Footer/>
   </React.StrictMode>
 );
 
